2c3aa8977948a0318ae9c88eb12ac1fcda61a210
Jimmy <jimmy.nguyen64@gmail.com>
2018-04-26 12:51:09 -0600

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Psychic-Game
+
+The Psychic Game lets you see if you are a true psychic. Take this challenging game to if you are the one. 
+
+Choose a letter from A - Z and test your psycic ability to see if you can read a computers mind! GOOD LUCK!!!
---
+++ asset/javascript/game.js
@@ -0,0 +1,69 @@
+// computer choices
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var guessesLeft = 9;
+var guessedLetters = [];
+var letterToGuess = null;
+
+
+
+
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+
+var updateGuessesLeft = function() {
+  
+  document.querySelector('#guessLeft').innerHTML = "Guesses left: " + guessesLeft;
+};
+
+var updateLetterToGuess = function() {
+  this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
+};
+var updateGuessesSoFar = function() {
+   
+  document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
+};
+
+var reset = function() {
+  totalGuesses = 9;
+  guessesLeft = 9;
+  guessedLetters = [];
+
+  updateLetterToGuess();
+  updateGuessesLeft();
+  updateGuessesSoFar();
+}
+
+updateLetterToGuess();
+updateGuessesLeft();
+
+
+// what happens when the key is press
+document.onkeyup = function(event) {
+    guessesLeft--;
+  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+  guessedLetters.push(userGuess);
+  updateGuessesLeft();
+  updateGuessesSoFar();
+
+        if (guessesLeft > 0){
+            if (userGuess == letterToGuess){
+                wins++;
+                document.querySelector('#wins').innerHTML = "Wins: " + wins;
+                alert("Yes, you're the best!!!");
+                reset();
+            }
+        }else if(guessesLeft == 0){
+             
+            losses++;
+            document.querySelector('#losses').innerHTML = "Losses: " + losses;
+            alert("Sorry, better luck next time.");
+            
+            reset();
+        }
+};
